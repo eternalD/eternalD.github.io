b738ec7cc8d2be410b1f458d5ca96316ae186fc7
Harri Kilpiö <harri.kilpio@gmail.com>
2015-05-25 13:10:22 +0300

    filter navi

--- proto/js/main.js
+++ proto/js/main.js
@@ -1,5 +1,31 @@
 jQuery(document).ready(function($){
 	var $timeline_block = $('.cd-timeline-block');
+  var $filter_picture = $('.filter-picture');
+  var $filter_movie = $('.filter-movie');
+  var $filter_location = $('.filter-location');
+
+  $filter_picture.click(function() {
+  	$(this).parent().parent().find('.cd-timeline-block').removeClass('is-hidden');
+  	$(this).parent().parent().find('.cd-timeline-picture').removeClass('is-hidden');
+  	$(this).parent().parent().find('.cd-timeline-movie').addClass('is-hidden');
+  	$(this).parent().parent().find('.cd-timeline-location').addClass('is-hidden');
+  });
+
+  $filter_movie.click(function() {
+  	
+  	$(this).parent().parent().find('.cd-timeline-block').removeClass('is-hidden');
+  	$(this).parent().parent().find('.cd-timeline-picture').addClass('is-hidden');
+  	$(this).parent().parent().find('.cd-timeline-movie').removeClass('is-hidden');
+  	$(this).parent().parent().find('.cd-timeline-location').addClass('is-hidden');
+  });
+
+  $filter_location.click(function() {
+  	
+  	$(this).parent().parent().find('.cd-timeline-block').removeClass('is-hidden');
+  	$(this).parent().parent().find('.cd-timeline-picture').addClass('is-hidden');
+  	$(this).parent().parent().find('.cd-timeline-movie').addClass('is-hidden');
+  	$(this').parent().parent().find('.cd-timeline-location').removeClass('is-hidden');
+  });
 
 	//hide timeline blocks which are outside the viewport
 	$timeline_block.each(function(){
@@ -7,6 +33,8 @@ jQuery(document).ready(function($){
 			$(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
 		}
 	});
+
+
 
 	//on scolling, show/animate timeline blocks when enter the viewport
 	$(window).on('scroll', function(){
@@ -16,4 +44,6 @@ jQuery(document).ready(function($){
 			}
 		});
 	});
+
+
 });
